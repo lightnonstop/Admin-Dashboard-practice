2d9539b9a1dac8ce0839718483d67502c64aa9b2
lightnonstop <umichaeledet003@gmail.com>
2023-06-21 05:48:30 +0100

    refactor cart model

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 var cartSchema = new mongoose.Schema({
-    products:[{
-        product:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-        },
-        count:Number,
-        color:String,
-        price: Number,
-    }],
-    cartTotal: Number,
-    totalAfterDiscount: Number,
-    orderBy:{
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    color: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Color',
     },
 }, { timestamps: true, });
 
