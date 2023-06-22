ec6eb23e8300ad37db2cd480b3cf0b1eced6b65f
lightnonstop <umichaeledet003@gmail.com>
2023-06-22 13:35:30 +0100

    update order fields

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,30 +1,32 @@
 const mongoose = require('mongoose');
-var orderSchema = new mongoose.Schema({
-    products:[{
-        product:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-        },
-        count:Number,
-        color:String,
+var orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    shippingInfo: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      other: { type: String, required: true },
+      zipcode: { type: String, required: true },
+    },
+    paymentInfo: {
+        razorpayOrderId: { type: String, required: true, },
+        razorpayPaymentId: { type: String, required: true, },
+    },
+    orderItems: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true, },
+        color: { type: mongoose.Schema.Types.ObjectId, ref: 'Color', required: true, },
+        quantity: { type: Number, required: true, },
+        price: { type: Number, required: true, },
     }],
-    paymentIntent:{},
-    orderStatus:{
-        type: String,
-        default: 'Not processed',
-        enum: [
-        'Not processed', 
-        'Cash on delivery', 
-        'Processing',
-        'Dispatched',
-        'Cancelled',
-        'Delivered'
-    ]
-    },
-    orderBy:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
-    },
-}, { timestamps: true, });
+    paidAt: { type: Date, default: Date.now(), },
+    totalPrice: { type: Number, required: true, },
+    totalPriceAfterDiscount: { type: Number, required: true, },
+    orderStatus: { type: String, default: 'Ordered', },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Order', orderSchema);
